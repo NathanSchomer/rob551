b30729247e505d2fc649bcd9649d4bbcce124456
Nathan Schomer <nathan@nathanschomer.com>
2023-02-28 22:01:07 -0800

    done with hw2 himm

---
+++ hw2/src/himm.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import LaserScan, Image
+from geometry_msgs.msg import Twist, PoseWithCovariance
+from nav_msgs.msg import Odometry
+from math import tanh
+import numpy as np
+from PIL import Image as PImage
+from pathlib import Path
+from math import radians, degrees
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from math import cos, sin
+from tf.transformations import euler_from_quaternion
+
+
+class HIMM:
+    def __init__(self, map_path):
+
+        rospy.init_node('himm', anonymous=True)
+
+        # load map
+        assert Path(map_path).is_file()
+        self.map_img = np.asarray(PImage.open(map_path))
+        self.map_dims = (300, 300)
+
+        # initialize map to all zeros
+        self.map = np.zeros(self.map_dims, dtype=np.uint8)
+
+        # subscribe to lidar and pose data
+        self.pose = None
+        self.sub_pose = rospy.Subscriber('base_pose_ground_truth', Odometry, self.pose_callback)
+        self.sub_lidar = rospy.Subscriber('base_scan', LaserScan, self.lidar_callback)
+        self.pub_map = rospy.Publisher('map', Image, queue_size=10)
+
+        self.INC_VAL, self.DEC_VAL = 3, -1
+        self.CELL_MIN, self.CELL_MAX = 0, 15
+        self.POSE_OFFSET = 8
+        self.SCALE_FACTOR = 10
+        self.MAX_RANGE = 8
+
+        self.thetas = []
+
+        self.bridge = CvBridge()
+
+    def pose_callback(self, msg):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        quat = msg.pose.pose.orientation
+        yaw = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[2]
+        self.pose = (round((x+self.POSE_OFFSET)*self.SCALE_FACTOR),
+                     round((y+self.POSE_OFFSET)*self.SCALE_FACTOR),
+                     yaw) 
+
+    def mod_map(self, idxs: set, val: int):
+        for idx in idxs:
+            new_val = int(self.map[idx]) + val
+            if new_val > self.CELL_MAX:
+                new_val = self.CELL_MAX
+            if new_val < self.CELL_MIN:
+                new_val = self.CELL_MIN
+            self.map[idx] = new_val
+
+    def init_thetas(self, data):
+        self.thetas = np.linspace(data.angle_min,
+                                  data.angle_max, 
+                                  len(data.ranges))
+
+    def inc_map(self, idxs: set):
+        self.mod_map(idxs, self.INC_VAL)
+
+    def dec_map(self, idxs: set):
+        self.mod_map(idxs, self.DEC_VAL)
+
+    def calc_endpoint(self, lidar_theta, range, pose):
+        theta = lidar_theta + pose[2]
+        x = pose[0] + int(cos(theta)*range)
+        y = pose[1] + int(sin(theta)*range)
+        return (x, y)
+
+    def calc_empty_cells(self, pose, endpoint):
+        # use https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm to calc empty cells
+        idxs = set()
+        x0, y0 = pose[0:2]
+        x1, y1 = endpoint
+
+        if x1 < x0:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx, dy = x1-x0, y1-y0
+        D = 2*dy - dx
+        y = y0
+
+        for x in range(x0, x1):
+            if x == x0 or x == x1:
+                continue
+            idxs.add((x, y))
+            if D > 0:
+                y += 1
+                D -= 2*dx
+            D += 2*dy
+        return idxs
+
+    def publish_map(self):
+        img = self.map.astype(np.uint8)
+        img[img == 0] = 255
+        img[img == 15] = 0
+        img = np.stack([img]*3, axis=-1)
+        idxs = np.transpose(np.where(np.any(0 < img, axis=2) & 
+                                     np.any(img < 255, axis=2)))
+        for idx in idxs:
+            img[idx[0], idx[1], 1:2] = 0
+            img[idx[0], idx[1], 0] = 255
+
+        cv2.imwrite('/media/psf/Home/Desktop/img.png', img)
+        ros_img = self.bridge.cv2_to_imgmsg(img)
+        self.pub_map.publish(ros_img)
+
+    def lidar_callback(self, lidar_data):
+        if len(self.thetas) == 0:
+            self.init_thetas(lidar_data)
+        pose = self.pose
+        inc_idxs, dec_idxs = set(), set()
+        for theta, range in zip(self.thetas, lidar_data.ranges):
+            range *= self.SCALE_FACTOR
+            endpoint = self.calc_endpoint(theta, range, pose)
+            if range < self.MAX_RANGE * self.SCALE_FACTOR:
+                inc_idxs.add(endpoint)
+            dec_idxs.union(self.calc_empty_cells(pose, endpoint))
+        self.inc_map(inc_idxs)
+        self.dec_map(dec_idxs)
+        self.publish_map()
+
+
+if __name__ == '__main__':
+    map_path = '/home/nathan/catkin_ws/src/rob551/stage_osu/config/simple_rooms.png'
+    himm = HIMM(map_path)
+    rospy.spin()
